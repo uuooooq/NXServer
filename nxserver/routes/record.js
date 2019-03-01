b2f0a7ec0086f108bd0da535097408ff1024ca57
Wenter.Zhu <Wenter.Zhu@connext.com.cn>
2019-03-01 17:31:34 +0800

    删除无用的代码

--- nxserver/routes/record.js
+++ nxserver/routes/record.js
@@ -3,55 +3,37 @@ var router = express.Router();
 var mysql = require('mysql');
 var mysqlinfo = require('../db/mysql');
 
-// var sqlite3 = require('sqlite3').verbose();
-
-// var db = new sqlite3.Database('./db/nx.db');
-//var db = new sqlite3.Database('./nx.db');
-
-
-
-/* GET home page. */
-
 router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-  results = [];
-//   db.all("SELECT * from record", function(err, rows){
-//       rows.map((row)=>{
-//       results.push({"recordid":row.recordid,
-//                 "content":row.content,
-//                 "createtime":row.createtime,
-//                 "contenttype":row.contenttype,
-//                 "contenttoken":row.contenttoken,
-//             })
-//       });
-//       res.json(results);
-//      });
-    //res.json(recorddao.findAllRecord())
-    var conn = mysql.createConnection(mysqlinfo.mysql);      //创建一个连接        mysql是我们上面文件暴露出来的模板的方法
-    conn.connect();
 
-    conn.query('select  * from record', function(err, rows, fields) {
+  results = [];
+  var conn = mysql.createConnection(mysqlinfo.mysql);      //创建一个连接        mysql是我们上面文件暴露出来的模板的方法
+  conn.connect();
+
+  conn.query('select  * from record', function(err, rows, fields) {
         if (err) throw err;
         //console.log('The solution is: ', rows);
         res.json(rows);
-    });
+  });
   
-    conn.end();
+  conn.end();
 });
 
-// router.post('/',function(req, res, next){
-//     db.run("insert into record(content,contenttype,contenttoken) VALUES (?,?,?)",req.body.content,req.body.type,req.body.token,function(err){
-       
-//         if(err){
-//             results = [{status:err.message}];
-//             res.json(results);
-//        }
-//        else{
-//             results = [{status:'success'}];
-//             res.json(results);
-//        }
-
-//     });
-// });
+router.post('/',function(req, res, next){
+  if(!req.body.content){
+    res.json({status:'error',desc:'content is null,can not be inserted'})
+  }
+  else{
+    var conn = mysql.createConnection(mysqlinfo.mysql);      
+    conn.connect();
+    conn.query('insert into record(content,contenttype) values(?,?)',[req.body.content,req.body.type],function(err){
+      if(err){
+        results = [{status:err.message}];
+        res.json(results);
+      }
+      res.json({status:'success'});
+    });
+    conn.end();
+  }
+});
 
 module.exports = router;
